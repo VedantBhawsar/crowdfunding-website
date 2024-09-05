43c95569d8eb8fd1512399bd4a3430938fce08ab
Vedant bhavsar <vedantbhavsar.a10@gmail.com>
2024-09-05 09:05:29 +0530

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,23 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={"outline"}>Button</Button>
-      <Button variant={"destructive"}>Button</Button>
-      <Button variant={"ghost"}>Button</Button>
-      <Button variant={"link"}>Button</Button>
-      <Button variant={"default"}>Button</Button>
-      <Button variant={"secondary"}>Button</Button>
+    <main className=" flex justify-center  w-full h-full">
+      <section className="mt-32 flex flex-col  items-center">
+        <h1 className="text-primary text-4xl font-bold">
+          Crowdfunding website
+        </h1>
+        <p className="text-slate-300 w-3/4 text-center mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non facilis
+          qui dicta totam vero natus reprehenderit placeat ducimus excepturi
+          sed! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+          temporibus aperiam soluta voluptate sit officia error, sint
+          consequuntur doloribus, ipsa non tenetur, hic quisquam? Pariatur
+          molestiae ex praesentium repellendus hic?
+        </p>
+        <Button variant={"outline"} className="mt-5">
+          Start Funding
+        </Button>
+      </section>
     </main>
   );
 }
